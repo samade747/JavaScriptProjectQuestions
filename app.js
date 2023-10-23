0c3c51d597e99ee2ebb5a820c3a2b8ee59622bcf
samade747 <samad.e747@gmail.com>
2023-10-23 21:26:04 +0500

    6565345

---
+++ app.js
@@ -0,0 +1,11 @@
+let body = document.getElementById('body');
+let heading = document.getElementById('heading');
+
+let box1 = document.querySelector('.box1');
+let box1inner = document.getElementById('box1-inner');
+let plusd = document.querySelector('.plus');
+let minusd = document.querySelector('.minus');
+console.log(minusd);
+let box1para = document.querySelector('.box1-para');
+minusd.style.display = 'none';
+box1para.style.display = 'none';
