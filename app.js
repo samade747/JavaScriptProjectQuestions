2dd45f7923053002bc32c0404d9b8601d658cd0c
samade747 <samad.e747@gmail.com>
2023-10-23 22:29:39 +0500

    878787

--- app.js
+++ app.js
@@ -10,6 +10,28 @@ let box1para = document.querySelector('.box1-para');
 minusd.style.display = 'none';
 box1para.style.display = 'none';
 
+
+let box2 = document.querySelector('.box2');
+let box2inner = document.getElementById('box2-inner');
+let plusd2 = document.querySelector('.plus2');
+let minusd2 = document.querySelector('.minus2');
+console.log(minusd2);
+let box2para = document.querySelector('.box2-para');
+minusd2.style.display = 'none';
+box2para.style.display = 'none';
+
+let box3 = document.querySelector('.box3');
+let box3inner = document.getElementById('box3-inner');
+let plusd3 = document.querySelector('.plus3');
+let minusd3 = document.querySelector('.minus3');
+console.log(minusd3);
+let box3para = document.querySelector('.box3-para');
+minusd3.style.display = 'none';
+box3para.style.display = 'none';
+
+
+
+
 function plus1() {
     minusd.style.display = 'inline';
     box1para.style.display = 'inline';
@@ -18,6 +40,11 @@ function plus1() {
     minusd3.style.display = 'none';
     box3para.style.display = 'none';
     plusd3.style.display = 'inline';
+
+    minusd2.style.display = 'none';
+    box2para.style.display = 'none';
+    plusd2.style.display = 'inline';
+
 
     minusd2.style.display = 'none';
     box2para.style.display = 'none';
@@ -34,14 +61,7 @@ function minus1() {
 
 
 
-let box2 = document.querySelector('.box2');
-let box2inner = document.getElementById('box2-inner');
-let plusd2 = document.querySelector('.plus2');
-let minusd2 = document.querySelector('.minus2');
-console.log(minusd2);
-let box2para = document.querySelector('.box2-para');
-minusd2.style.display = 'none';
-box2para.style.display = 'none';
+
 
 
 function plus2() {
@@ -71,14 +91,7 @@ function minus2() {
 
 
 
-let box3 = document.querySelector('.box3');
-let box3inner = document.getElementById('box3-inner');
-let plusd3 = document.querySelector('.plus3');
-let minusd3 = document.querySelector('.minus3');
-console.log(minusd3);
-let box3para = document.querySelector('.box3-para');
-minusd3.style.display = 'none';
-box3para.style.display = 'none';
+
 
 
 function plus3() {
